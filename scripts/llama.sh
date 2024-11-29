b3e5d640bbc31a29266dff0a958ad7f7fdf7e3c4
AleHD <alejandro.hernandezcano@epfl.ch>
2024-11-29 12:09:58 +0100

    new llama script

--- scripts/llama.sh
+++ scripts/llama.sh
@@ -1,27 +1,35 @@
 #= PRELUDE: Command line utilities and handling the model size =#
-SCRIPT_VERSION=v2
 TODI_GPUS=4
 RCP_GPUS=8
+SCRIPT_VERSION=v3
 
 # Print usage function.
 usage () {
 	echo "Usage: llama.sh <size> [options...]"
-	echo "<size>: 1/3/8"
+	echo "<size>: 1/3/8/70"
 	echo "Options:"
 	echo " --help: Displays this message"
-	echo " --todi: Use todi paths and launch using slurm"
+	echo " --rcp: Use rcp paths and launch using cmd, not slurm"
 	echo " --fp8: Enables fp8"
+	echo " --fp8-margin: Enables fp8"
 	echo " --fp8-dpa: Enables fp8 DPA"
 
 	echo " --op: Enables outlier protected block"
+	echo " --torch-qknorm: Enables torch qk norm"
 	echo " --correct-beta: Use the correct 1/sqrt(layers) beta instead of the 1/layers"
 	echo " --single-scaler: Use only one residual scaler (instead of a hidden_size weight)"
+	echo " --no-downscale: Dont use residual downscaling at all"
 	echo " --with-final-ln: Enables final layernorm in outlier protected block"
 	echo " --gelu: Use gelu (instead of relu)"
 	echo " --swiglu: Use swiglu (instead of relu)"
 	echo " --shallow: Don't widen FFN with OP block"
-	echo " --input-scaling: Enables scaling input by 1/init_std"
+	echo " --no-input-scaling: Enables scaling input by 1/init_std"
 	echo " --no-remove-interln: Don't remove the mlp and attention layernorms"
+
+	echo " --debug: Mark run as debug script version"
+	echo " --nodes <nodes>: How many nodes to use"
+	echo " --goodnodes <nodes>: Specify the good node list"
+	echo " --reservation <reservation>: Todi reservation to use"
 
 	echo " --decay <decay>: Specify weight decay"
 	echo " --lr <lr>: Specify learningrate"
@@ -39,30 +47,29 @@ if [[ $# -eq 0 ]]; then
 fi
 
 if [[ $1 -eq 1 ]]; then 
-	# batch_size = 1.31 M
-	# total_tokens = ~78.64B tokens
-	# todi time per iter (1 node): 7.5s
-	# todi ETA (1 node): 5d5h
+	# batch_size = 1.47 M
+	# total_tokens = ~79.62B tokens
+	# todi time per iter (1 node): 9.4s
+	# todi ETA (1 node): 5d21h
 	TP=1
-	LAYERS=14
-	HIDDEN_SIZE=2048
-	FFN_SIZE=8192
+	PP=1
+	LAYERS=24
+	HIDDEN_SIZE=1536
+	FFN_SIZE=6144
 	NUM_HEADS=16
 	NUM_QUERY_GROUPS=16
-	MBS=4
-	GBS=160
-	#ITERS=60000
-	ITERS=100
+	MBS=3
+	GBS=180
+	ITERS=54000
 	LR=0.001
-	MINLR=1e-5
-	DECAY=0.01
-	INIT_STD=0.02
+	INIT_STD=0.025
 	SIZE=1
 	SAVE_FREQ=250
 elif [[ $1 -eq 3 ]]; then
 	# batch_size = 180*8192 = ~1.47M tokens
 	# total_tokens = ~14.75B
 	TP=2
+	PP=1
 	LAYERS=36
 	HIDDEN_SIZE=2048
 	FFN_SIZE=8192
@@ -72,35 +79,51 @@ elif [[ $1 -eq 3 ]]; then
 	GBS=180
 	ITERS=10000
 	LR=0.001
-	MINLR=1e-5
-	DECAY=0.1
 	INIT_STD=0.02
 	SIZE=3
 	SAVE_FREQ=200
 elif [[ $1 -eq 8 ]]; then
-	# batch_size = ~2.46M
-	# total_tokens = ~9.83B
+	# batch_size = ~2.1M
+	# total_tokens = ~52.42B
 	TP=4
+	PP=1
 	LAYERS=32
 	HIDDEN_SIZE=4096
 	FFN_SIZE=14336
 	NUM_HEADS=32
 	NUM_QUERY_GROUPS=8
 	MBS=2
-	GBS=300
-	ITERS=4000
+	GBS=256
+	ITERS=25000
 	LR=0.0005
-	MINLR=1e-5
-	DECAY=0.1
 	INIT_STD=0.01
 	SIZE=8
-	SAVE_FREQ=100
+	SAVE_FREQ=1000
+elif [[ $1 -eq 70 ]]; then
+	# batch_size = ~4.19M
+	# total_tokens = ~20.97B
+	TP=4
+	PP=8
+	LAYERS=80
+	HIDDEN_SIZE=8192
+	FFN_SIZE=28672
+	NUM_HEADS=64
+	NUM_QUERY_GROUPS=8
+	MBS=1
+	GBS=512
+	ITERS=5000
+	LR=0.00008
+	INIT_STD=0.01
+	SIZE=70
+	SAVE_FREQ=500
 else
 	echo "Invalid llama size: $1"
 	usage
 	exit 1
 fi
 shift
+DECAY=0.1
+MINLR=1e-8
 
 NEW_LR=""
 NEW_MINLR=""
@@ -108,20 +131,27 @@ NEW_DECAY=""
 FP8=false
 FP8DPA=false
 OP=false
+TORCH_QKNORM=false
 FINAL_LN=false
 GELU=false
 SHALLOW=false
-INPUT_SCALING=false
+INPUT_SCALING=true
 CORRECT_BETA=false
 REMOVE_INTERLN=true
+DOWNSCALE=true
 SWIGLU=false
-TODI=false
+TODI=true
 SINGLE_SCALER=false
 EXTRA_NAME=""
 WANDB_ID=""
 SCHEDULER=wsd
+
+GOOD_NODES=""
 NODES=1
 REQ_NODES=1
+RESERV=""
+
+FP8_MARGIN=0
 
 ADAM_EPS=0.00000001
 NORM_EPS=0.00001
@@ -133,14 +163,20 @@ while [[ $# -gt 0 ]]; do
 			usage; exit 0;;
 		--fp8)
 			FP8=true; shift;;
-		--todi)
-			TODI=true; shift;;
+		--fp8-margin)
+			FP8_MARGIN=$2; shift 2;;
+		--rcp)
+			TODI=false; shift;;
 		--fp8-dpa)
 			FP8DPA=true; shift;;
 		--op)
 			OP=true; shift;;
+		--torch-qknorm)
+			TORCH_QKNORM=true; shift;;
 		--correct-beta)
 			CORRECT_BETA=true; shift;;
+		--no-downscale)
+			DOWNSCALE=false; shift;;
 		--single-scaler)
 			SINGLE_SCALER=true; shift;;
 		--with-final-ln)
@@ -153,12 +189,20 @@ while [[ $# -gt 0 ]]; do
 			GELU=true; shift;;
 		--swiglu)
 			SWIGLU=true; shift;;
+
 		--nodes)
 			REQ_NODES=$2; shift 2;;
+		--goodnodes)
+			GOOD_NODES=$2; shift 2;;
+		--debug)
+			SCRIPT_VERSION=$SCRIPT_VERSION-debug; shift;;
+		--reservation)
+			RESERV=$2; shift 2;;
+
 		--shallow)
 			SHALLOW=true; shift;;
-		--input-scaling)
-			INPUT_SCALING=true; shift;;
+		--no-input-scaling)
+			INPUT_SCALING=false; shift;;
 		--no-remove-interln)
 			REMOVE_INTERLN=false; shift;;
 		--lr)
@@ -193,14 +237,17 @@ fi
 
 #= MIDDLE: Set up arguments depending on the commandline =#
 ENVS=""
-if [ $TODI = true ]; then
-	SUFFIX=$SUFFIX-edu; 
+if [ $TODI = false ]; then
+	SUFFIX=$SUFFIX-rcp; 
 fi
 
 FP8_ARGS=""
 if [ $FP8 = true ]; then
 	SUFFIX=$SUFFIX-fp8
-	FP8_ARGS="$FP8_ARGS --fp8-format hybrid --fp8-amax-history-len 1024 --fp8-amax-compute-algo max"
+	FP8_ARGS="$FP8_ARGS --fp8-margin $FP8_MARGIN --fp8-format hybrid --fp8-amax-history-len 1024 --fp8-amax-compute-algo max"
+	if [ $FP8_MARGIN -ne 0 ]; then
+		SUFFIX=$SUFFIX-fp8margin$FP8_MARGIN
+	fi
 	if [ $FP8DPA = true ]; then
 		SUFFIX=$SUFFIX-fp8dpa
 		FP8_ARGS="$FP8_ARGS --fp8-dot-product-attention"
@@ -209,7 +256,12 @@ fi
 
 if [ $OP = true ]; then
 	SUFFIX=$SUFFIX-op
-	OP_ARGS="--qk-layernorm --log-scaler-norm"
+	OP_ARGS="--qk-layernorm"
+
+	if [ $TORCH_QKNORM = true ]; then
+		OP_ARGS="$OP_ARGS --use-torchqknorm --no-persist-layer-norm"
+		SUFFIX=$SUFFIX-torchqk
+	fi
 
 	if [ $REMOVE_INTERLN = true ]; then
 		OP_ARGS="$OP_ARGS --no-attn-layernorm --no-mlp-layernorm"
@@ -229,17 +281,21 @@ if [ $OP = true ]; then
 		OP_ARGS="$OP_ARGS --no-final-layernorm"
 	fi
 
-	if [ $CORRECT_BETA = true ]; then
-		SUFFIX=$SUFFIX-correctbeta
-		BETA=$(echo "print(1/$LAYERS**0.5)" | python)
-	else
-		BETA=$(echo "print(1/$LAYERS)" | python)
-	fi
-	OP_ARGS="$OP_ARGS --downscale-residual $BETA"
+	if [ $DOWNSCALE = true ]; then
+		if [ $CORRECT_BETA = true ]; then
+			SUFFIX=$SUFFIX-correctbeta
+			BETA=$(echo "print(1/$LAYERS**0.5)" | python3)
+		else
+			BETA=$(echo "print(1/$LAYERS)" | python3)
+		fi
+		OP_ARGS="$OP_ARGS --downscale-residual $BETA --log-gains-norm"
 
-	if [ $SINGLE_SCALER = true ]; then
-		SUFFIX=$SUFFIX-1scaler
-		OP_ARGS="$OP_ARGS --single-downscale-residual"
+		if [ $SINGLE_SCALER = true ]; then
+			SUFFIX=$SUFFIX-1scaler
+			OP_ARGS="$OP_ARGS --single-residual-gain"
+		fi
+	else
+		SUFFIX=$SUFFIX-nodownscale
 	fi
 
 	if [ $GELU = true ]; then
@@ -252,9 +308,10 @@ if [ $OP = true ]; then
 	fi
 
 	if [ $INPUT_SCALING = true ]; then
-		MULT=$(echo "print(1/$INIT_STD)" | python)
+		MULT=$(echo "print(1/$INIT_STD)" | python3)
 		OP_ARGS="$OP_ARGS --input-embeddings-multiplier $MULT"
-		SUFFIX=$SUFFIX-inputscale
+	else
+		SUFFIX=$SUFFIX-noinputscale
 	fi
 
 else
@@ -278,8 +335,8 @@ fi
 if [ $SCHEDULER = wsd ]; then
 	SCHEDULER_ARGS=(
 		--lr-decay-style WSD
-		--lr-wsd-decay-style linear
-		--lr-wsd-decay-iters $(($ITERS/10))
+		--lr-wsd-decay-style 1-sqrt
+		--lr-wsd-decay-iters $(($ITERS/5))
 		--lr-warmup-iters $(($ITERS/20))
 	)
 else
@@ -291,6 +348,10 @@ else
 	)
 fi
 
+if [ $REQ_NODES -ne 1 ]; then
+	SUFFIX=$SUFFIX-nodes$REQ_NODES
+fi
+
 SUFFIX=$SUFFIX$EXTRA_NAME
 NAME=llama${SIZE}b$SUFFIX
 
@@ -298,7 +359,7 @@ if [ $TODI = true ]; then
 	GPUS=$TODI_GPUS
 	# 81.816 B tokens
 	DATA_PATH=/store/swissai/a06/users/ahernnde/data/finewebedu-sample-100BT/megatrontokenized/finewebedu-llama3tok_text_document
-	SAVE_PATH=/store/swissai/a06/users/ahernnde/checkpoints/megatron/fp8experiments_$SCRIPT_VERSION/$NAME
+	SAVE_PATH=/capstor/scratch/cscs/ahernnde/checkpoints/megatron/fp8experiments_$SCRIPT_VERSION/$NAME
 	TOKENIZER=/store/swissai/a06/models/Meta-Llama-3.1-8B/
 	NODES=$REQ_NODES
 else
@@ -320,25 +381,24 @@ ENVS="$ENVS CUDA_DEVICE_MAX_CONNECTIONS=1 OMP_NUM_THREADS=64"
 WANDB_PROJECT=megatron_fp8_experiments_rcp_$SCRIPT_VERSION
 SEQ_LEN=8192
 
-if [ "$WANDB_ID" == "" ]; then
+if [ "$WANDB_ID" = "" ]; then
 	ENVS="$ENVS WANDB_RESUME=allow WANDB_RUN_ID=${WANDB_PROJECT}_$NAME"
 else
-	ENVS="$ENVS WANDB_RESUME=must WANDB_RUN_ID=$WANDB_ID"
+	ENVS="$ENVS WANDB_RESUME=allow WANDB_RUN_ID=$WANDB_ID"
 fi
 
 
 LLAMA_ARGS=(
 	--tensor-model-parallel-size $TP
-	--pipeline-model-parallel-size 1
+	--pipeline-model-parallel-size $PP
 	--seq-length $SEQ_LEN
-	--max-position-embeddings 131072
+	--max-position-embeddings $SEQ_LEN
 	--tokenizer-type HuggingFaceTokenizer
 	--tokenizer-model $TOKENIZER
 	--exit-on-missing-checkpoint
 	--untie-embeddings-and-output-weights
 	--normalization RMSNorm
 	--position-embedding-type rope
-	--no-masked-softmax-fusion
 	--attention-softmax-in-fp32
 	--disable-bias-linear
 	--transformer-impl transformer_engine
@@ -402,6 +462,9 @@ LOGGING=(
 
 EXTRA_ARGS=(
 	--use-distributed-optimizer
+	--overlap-grad-reduce
+	--overlap-param-gather
+	--async-save
 )
 
 MAYBE_LOAD=""
@@ -413,45 +476,61 @@ ARGS="${LLAMA_ARGS[@]} ${TRAINING_ARGS[@]} ${SCHEDULER_ARGS[@]} ${DATA_ARGS[@]} 
 
 #= RUNNING: Run the command, or launch a slurm script if using todi =#
 if [ $TODI = true ]; then
-	DISTRIBUTED_ARGS="${DISTRIBUTED_ARGS[@]} --master-addr=\$MASTER_ADDR --node-rank=\$SLURM_PROCID"
+	DISTRIBUTED_ARGS="${DISTRIBUTED_ARGS[@]} --master-addr=\$MASTER_ADDR --node-rank=\\\$SLURM_PROCID"
 	CMD="torchrun $DISTRIBUTED_ARGS pretrain_gpt.py $ARGS"
 
+	if [ "$RESERV" != "" ]; then
+		RESERV_STRING="#SBATCH --reservation=$RESERV"
+	fi
+	if [ "$GOOD_NODES" != "" ]; then
+		GOOD_NODES_STRING="#SBATCH --nodelist=$GOOD_NODES"
+	fi
+
 	mkdir -p $SAVE_PATH
-	#SBATCH --time=6:00:00
-	#SBATCH --environment=/store/swissai/a06/users/ahernnde/container-image/nemo-swissai/nemo-swissai.toml
 	cat > $SAVE_PATH/submission.sbatch <<- EOM
 	#!/bin/bash
 	#SBATCH --account=a06
 	#SBATCH --cpus-per-task=288
-	#SBATCH --environment=/store/swissai/a06/users/ahernnde/container-image/megatron/megatron.toml
 	#SBATCH --gres=gpu:4
+	#SBATCH --environment=/store/swissai/a06/users/ahernnde/container-image/nemo-swissai/nemo-swissai-oldimg.toml
 	#SBATCH --job-name=$NAME
 	#SBATCH --mem=460000
 	#SBATCH --nodes=$NODES
 	#SBATCH --ntasks-per-node=1
 	#SBATCH --output=$SAVE_PATH/slurmlogs/$NAME_%j.out
 	#SBATCH --error=$SAVE_PATH/slurmlogs/$NAME_%j.err
-	#SBATCH --time=0:30:00
-	#SBATCH --partition=debug
+	#SBATCH --time=12:00:00
 	#SBATCH --exclusive
+	$RESERV_STRING
+	$GOOD_NODES_STRING
 
+	echo "Using nodes: \$SLURM_JOB_NODELIST"
+	srun -l bash -c 'echo \$(hostname) \$(nvidia-smi | grep -o "|\\s*[0-9]*MiB")'
+
+	export ENROOT_LIBRARY_PATH=/capstor/scratch/cscs/fmohamed/enrootlibxpmem
 	export WANDB_API_KEY=$(cat /store/swissai/a06/users/ahernnde/.keys/wandb.txt)
 	export MASTER_ADDR=\$(hostname)
+
+	# Log git status.
+	cd /store/swissai/a06/users/ahernnde/workspace/AleHD-Megatron-LM
+	git log --name-status HEAD^..HEAD
+	git diff
 
 	MAYBE_LOAD=""
 	if [ -f $SAVE_PATH/latest_checkpointed_iteration.txt ]; then
 		MAYBE_LOAD="--load $SAVE_PATH"
 	fi
 
+	python /store/swissai/a06/users/ahernnde/workspace/AleHD-Megatron-LM/scripts/remove_incomplete_checkpoint.py $SAVE_PATH
+
 	srun --unbuffered bash -c "
 		cd /store/swissai/a06/users/ahernnde/workspace/AleHD-Megatron-LM
-		pip install wandb
 		export PYTHONPATH=\$PWD
 		eval \"$ENVS\" $CMD \$MAYBE_LOAD
 	"
 	EOM
 	echo "Saved sbatch to $SAVE_PATH/submission.sbatch"
-	export ENROOT_LIBRARY_PATH=/capstor/scratch/cscs/fmohamed/enrootlibn
+	export ENROOT_LIBRARY_PATH=/capstor/scratch/cscs/fmohamed/enrootlibxpmem
 	sbatch $SAVE_PATH/submission.sbatch
 else
 	CMD="torchrun ${DISTRIBUTED_ARGS[@]} --master_addr localhost pretrain_gpt.py $ARGS $MAYBE_LOAD"

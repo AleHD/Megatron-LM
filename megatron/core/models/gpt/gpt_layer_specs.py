6cdca97f87b92e7337dfe4e5470c48004a75e622
AleHC <alejandro.hernandezcano@epfl.ch>
2024-11-29 12:44:53 +0100

    fixmerge

--- megatron/core/models/gpt/gpt_layer_specs.py
+++ megatron/core/models/gpt/gpt_layer_specs.py
@@ -7,7 +7,7 @@ from megatron.core.tensor_parallel.layers import ColumnParallelLinear, RowParall
 from megatron.core.transformer.attention import SelfAttention, SelfAttentionSubmodules
 from megatron.core.transformer.dot_product_attention import DotProductAttention
 from megatron.core.transformer.enums import AttnMaskType
-from megatron.core.transformer.torch_norm import WrappedTorchRMSNorm
+from megatron.core.transformer.torch_norm import WrappedTorchNorm
 from megatron.core.transformer.identity_op import IdentityOp
 from megatron.core.transformer.layer_scale import LayerScale
 from megatron.core.transformer.mlp import MLP, MLPSubmodules
@@ -112,7 +112,7 @@ def get_gpt_layer_with_transformer_engine_spec(
         # for QKLayerNorm if TE Version < 1.9;
         # we instead use the Apex implementation.
         qk_norm = TENorm if is_te_min_version("1.9.0") else FusedLayerNorm
-        qk_norm = WrappedTorchRMSNorm if use_torchqknorm else qk_norm
+        qk_norm = WrappedTorchNorm if use_torchqknorm else qk_norm
         return ModuleSpec(
             module=TransformerLayer,
             submodules=TransformerLayerSubmodules(
